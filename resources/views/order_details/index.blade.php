5f86f94e09701ea05c672a1531d67889b13917be
Ketan <ketanmangukiya001@gmail.com>
2024-10-12 17:52:20 +0530

    added tracking status

--- resources/views/order_details/index.blade.php
+++ resources/views/order_details/index.blade.php
@@ -8,7 +8,7 @@
                 <a href="{{ route('order_details.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
                     Create Order Detail
                 </a>
-                <button type="button" onclick="confirmDelete()" class="inline-flex items-center px-4 py-2 bg-red-600 text-white rounded-md">
+                <button type="button" onclick="confirmDelete()" class="inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 focus:bg-red-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
                     Delete Selected
                 </button>
             </div>
@@ -299,6 +299,16 @@
                                         </td>
                                         <td class="py-4 px-6 align-start text-center">
                                             <div class="inline-flex items-center space-x-4">
+                                            @if($orderDetail->trackingCompany && $orderDetail->trackingCompany->link && $orderDetail->tracking_number)
+                                            <a href="{{ str_replace('{tracking_number}', $orderDetail->tracking_number, $orderDetail->trackingCompany->link) }}" 
+                                                target="_blank" 
+                                                class="text-gray-600 hover:text-gray-800">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                                    </svg>
+                                                </a>
+                                            @endif
                                                 <a href="{{ route('order_details.edit', $orderDetail) }}">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
