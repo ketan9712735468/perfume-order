7f8d44567d3cd981ddc935886175183a5b6ecc9e
Ketan <ketanmangukiya001@gmail.com>
2025-07-23 00:53:33 +0530

    automatically redirect to /login

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+use Illuminate\Session\TokenMismatchException;
+
+class Handler extends ExceptionHandler
+{
+    protected $dontReport = [];
+
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    public function register(): void
+    {
+        //
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof TokenMismatchException) {
+            return redirect('/login')->with('message', 'Session expired. Please log in again.');
+        }
+
+        return parent::render($request, $exception);
+    }
+}
